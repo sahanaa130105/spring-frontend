9b4cb0af110e8e81addab879d7f746919b441820
sahanaa130105 <sahanaa.b2023aiml@sece.ac.in>
2025-07-18 09:08:56 +0530

    frontend 2

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import GetEmployees from "./components/GetEmployees";
+// import AddEmployee from "./components/AddEmployee";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<GetEmployees />} />
+      {/* <Route path="/add" element={<AddEmployee />} /> */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
